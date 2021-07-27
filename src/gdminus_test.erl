f163f0c9dd21ec668ade95a47fef76dc65dd0a01
Lincoln Bryant <comradek@gmail.com>
2021-07-27 00:14:51 -0500

    some more convenient test functions

--- src/gdminus_test.erl
+++ src/gdminus_test.erl
@@ -2,8 +2,9 @@
 
 -export([
     regenerate/0,
-	lex_file/1
-	%parse_file/1
+	lex_file/1,
+	parse_file/1,
+	interpret_file/1
 	]).
 
 lex_file(File) ->
@@ -11,6 +12,14 @@ lex_file(File) ->
 	Fn = binary:bin_to_list(F),
 	{ok, L, _Lines} = gdminus_scan:string(Fn),
 	gdminus_scan:normalize(L).
+
+parse_file(File) ->
+    {ok, AST} = gdminus_parse:parse(lex_file(File)),
+    AST.
+
+interpret_file(File) ->
+    AST = parse_file(File),
+    gdminus_interpreter:walk(AST).
 
 regenerate() ->
     leex:file("src/gdminus_scan.xrl"),
