c54f6e16e4fbfc80553f49cb7c83fda7b0a4bcfb
Lincoln Bryant <comradek@gmail.com>
2021-08-12 02:13:43 -0500

    gdscript equivalent of the gdminus code

---
+++ examples/fib.gd
@@ -0,0 +1,16 @@
+func _ready():
+    var start = time()
+    print(fib(30))
+    var end = time()
+    print("Time: " + str(end - start) + "ms")
+
+
+
+func fib(n):
+    if n < 2:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+func time():
+    return OS.get_ticks_msec()
